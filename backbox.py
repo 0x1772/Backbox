2aaf241068c0975322ca2eb95f104072f51a8546
0x1772 <realhateux@outlook.com>
2022-10-03 18:39:57 +0300

    update...

--- backbox.py
+++ backbox.py
@@ -1,2 +1,44 @@
-import os 
-os.system('python vulner.py')
+import sys
+import socket
+import subprocess
+import os
+import time
+import signal
+import random
+import string
+import threading
+import re
+
+def print_banner():
+    banner = """\
+        
+ ▄▄▄▄    ▄▄▄       ▄████▄   ██ ▄█▀ ▄▄▄▄    ▒█████  ▒██   ██▒
+▓█████▄ ▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█████▄ ▒██▒  ██▒▒▒ █ █ ▒░
+▒██▒ ▄██▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒██▒ ▄██▒██░  ██▒░░  █   ░
+▒██░█▀  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒██░█▀  ▒██   ██░ ░ █ █ ▒ 
+░▓█  ▀█▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▓█  ▀█▓░ ████▓▒░▒██▒ ▒██▒
+░▒▓███▀▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░▒▓███▀▒░ ▒░▒░▒░ ▒▒ ░ ░▓ ░
+▒░▒   ░   ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░▒░▒   ░   ░ ▒ ▒░ ░░   ░▒ ░
+ ░    ░   ░   ▒   ░        ░ ░░ ░  ░    ░ ░ ░ ░ ▒   ░    ░  
+ ░            ░  ░░ ░      ░  ░    ░          ░ ░   ░    ░  
+      ░           ░                     ░                   
+
+by 0x1772.
+
+type <help> for usage information!\n\n"""
+    for line in banner:
+        term.output(text.blue(line)) 
+
+
+
+def function(Optiox):
+    if Optiox == '1':
+        os.system('python2 vulscanner.py')
+    elif Optiox == '2':
+        os.system('python2 ipchanger.py')
+    else:
+        print("Please type an available function")
+
+function(input('Choose Option:'))
+
+print_banner()
