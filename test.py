6a8c20941edffe9ab0afc4883be204201b68bc22
0x1772 <realhateux@outlook.com>
2022-10-13 18:59:36 +0300

    test is testing commmands

---
+++ test.py
@@ -0,0 +1,9 @@
+##tesla stock data
+def stock_tesla_data_check():
+    import urllib.request, json
+    resp = urllib.request.urlopen('https://query2.finance.yahoo.com/v10/finance/quoteSummary/tsla?modules=price')
+    data = json.loads(resp.read())
+    price = data['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+    print(price)
+
+stock_tesla_data_check()
