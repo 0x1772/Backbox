2934abf16dee2cee6d8161750990d6ec1527b5a7
0x1772 <realhateux@outlook.com>
2022-10-15 18:31:16 +0300

    test is for teaching code

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import random
+
 ##tesla stock data
 def stock_tesla_data_check():
     import urllib.request, json
@@ -6,4 +8,29 @@ def stock_tesla_data_check():
     price = data['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
     print(price)
 
-stock_tesla_data_check()
+def iki_bilinmeyenli_denklem():
+     
+    print("2.Dereceden Bir Denklemin Kökünü Bulma")
+    #y=ax^2+bx+c
+ 
+    a=int(input("a : "))
+    b=int(input("b : "))
+    c=int(input("c : "))
+ 
+    delta=b**2-4*a*c
+ 
+    x1=(-b-delta**0.5)/(2*a)
+    x2=(-b+delta**0.5)/(2*a)
+ 
+    print("Birinci Kök : {}\nİkinci Kök : {}".format(x1,x2))
+
+def tri_recursion(k):
+  if(k>0):
+    result = k+tri_recursion(k-1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nÖzyineleme örnek sonuçlar")
+tri_recursion(5)
